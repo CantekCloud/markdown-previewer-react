bd5e4b2a7270fbb9a6fb8770b4d6041e6fbed2a3
Qasim Khan <69918464+QasimAK191@users.noreply.github.com>
2023-09-18 17:06:56 -0400

    Changes to Makrdown Guide

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 import NavigationBar from './components/Navbar.js';
 import MarkdownGuide from './components/MarkdownGuide.js';
 import MarkdownInput from './components/MarkdownInput.js';
-import MarkdownOutput from './components/MarkdownOutput.js'
+import MarkdownOutput from './components/MarkdownOutput.js';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-    const [markdown, setMarkdown] = useState(`# Hello World`)
-    const [showGuide, setShowGuide] = useState(false)
+    const [markdown, setMarkdown] = useState(`# Hello World`);
+    const [showGuide, setShowGuide] = useState(false);
+    const [guideSelection, setGuideSelection] = useState(' ');
 
     let onToggleGuide = () => {
         setShowGuide(!showGuide)
@@ -21,21 +22,26 @@ function App() {
         setMarkdown(event.target.value)
     }
 
+    let onClick = (event) => {
+        event.preventDefault();
+        setGuideSelection('\n'+event.target.value)
+        console.log(guideSelection)
+    }
+
   return (
       <div className="App">
           
           <div>
               <NavigationBar />
           </div>
-          <div className="padding" />
           <br />
           <br />
           <div className="toggle">
-              <MarkdownGuide showGuide={showGuide} onToggleGuide={onToggleGuide} />
+              <MarkdownGuide showGuide={showGuide} onToggleGuide={onToggleGuide} onClick={onClick}/>
           </div>
 
           <div className="input">
-              <MarkdownInput onChange={handleChange} value={markdown}/>
+              <MarkdownInput onChange={handleChange} value={markdown} onGuide={guideSelection}/>
           </div>
 
           <div className="output">
--- src/components/MarkdownGuide.js
+++ src/components/MarkdownGuide.js
@@ -1,32 +1,191 @@
 import { useState } from 'react';
-import { Button, Offcanvas, OffcanvasHeader, OffcanvasBody, OffcanvasBodyProps, OffcanvasHeaderProps, OffcanvasProps } from 'reactstrap';
+import { Button, Offcanvas, OffcanvasHeader, OffcanvasBody, OffcanvasBodyProps, OffcanvasHeaderProps, OffcanvasProps, ListGroup, ListGroupItem } from 'reactstrap';
 import { styled, css } from 'styled-components';
 
 
 import './MarkdownGuide.css';
 
+const myStyle = {
 
-export default function MarkdownGuide (props) {
+    color: 'pink',
+    background: 'rgba(10,20,30,1)',
+    opacity: '97%',
 
+}
+
+const myStyle2 = {
+
+    color: 'red',
+    font: 'Lucida Console',
+
+}
+
+const myStyle3 = {
+    background: '#dd1313',
+    borderradius: '15px',
+    border: '0px',
+}
+
+
+export default function MarkdownGuide(props) {
+
+   
 
     return (
         <div className="toggler">
             <div>
                 <Button
                     color="dark"
-                    onClick={props.showGuide}
+                    onClick={props.onToggleGuide}
+                    style={myStyle3}
                 >
                     Toggle Markdown Guide
                 </Button>
                 <Offcanvas
+                    backdrop={false}
+                    direction="end"
+                    fade={false}
+                    isOpen={props.showGuide}
+                    toggle={props.onToggleGuide}
+                    zIndex={4}
+                    style={myStyle}
                 >
-                    <OffcanvasHeader toggle={props.onToggleGuide}>
-                        Offcanvas
+                    <OffcanvasHeader isOpen={props.showGuide} toggle={props.onToggleGuide} style={myStyle2}>
+                        Markdown Guide
                      </OffcanvasHeader>
-                    <OffcanvasBody>
+                    <OffcanvasBody style={myStyle}>
                         <strong>
-                            This is the Offcanvas body.
+                            Here are a common list of Markdown tags
+                            <br />                          
                     </strong>
+
+                        <br />
+                        
+                        
+                        <table>
+                            <tr>
+                                <th>
+                                    Headings
+                                </th>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">#</td>
+                                <td><h1>Heading #1</h1></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">##</td>
+                                <td><h2>Heading #2</h2></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">###</td>
+                                <td><h3>Heading #3</h3></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">####</td>
+                                <td><h4>Heading #4</h4></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">#####</td>
+                                <td><h5>Heading #5</h5></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">######</td>
+                                <td><h6>Heading #6</h6></td>
+                            </tr>
+                        </table>
+
+                        <br />
+
+                        <table>
+                            <tr>
+                                <th>
+                                    Text Styling
+                                </th>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag2">&lt;p&gt; Paragraph 1 &lt;/p&gt; <br /> &lt;p&gt; Paragraph 2 &lt;/p&gt;</td>
+                                <td><div className=""><p>Paragraph 1</p> <p>Paragraph 2</p></div></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">* <i>your text</i> *</td>
+                                <td><div className="text-item"><b>Bold text</b></div></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">** <i>your text</i> **</td>
+                                <td><div className="text-item"><i>Italic text</i></div></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">*** <i>your text</i> ***</td>
+                                <td><div className="text-item"><b><i>Bold and Italic text</i></b></div></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag"> &gt; <i>your quote</i> </td>
+                                <td><div className="text-item"><blockquote>Your quote</blockquote></div></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">` <i>your code</i> `</td>
+                                <td><div className="text-item"><code>Your code</code></div></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">
+                                    ***
+                                    <br />
+                                    ---
+                                    <br />
+                                    _________________
+                                </td>
+                                <td><div className="">Horizontal line <br /><hr /></div></td>
+                            </tr>
+                        </table>
+
+                        <br />
+
+                        <table>
+                            <tr>
+                                <th>
+                                    Links and Images
+                                </th>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag2">
+                                    ![Image text](<i>www.yourimage.com/yourimage</i>)
+                                </td>
+                                <td><div className="text-item2"><img src={'https://1.bp.blogspot.com/-MJPIUGr6H2c/XnnFB3CKr2I/AAAAAAAAH0M/XZp196mRbNkM1-3fueRhvKsdv8r-2AmMwCLcBGAsYHQ/s1600/powerful-interview-tip-say-for-example.jpg' } /></div></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag2">
+                                    [Example link text](<i>www.yourlink.com</i>)
+                                </td>
+                                <td><div className="text-item"><a href={'#'}>Example link text</a></div></td>
+                            </tr>
+
+
+                        </table>
+                        <br />
+
+                        <table>
+                            <tr>
+                                <th>
+                                    Lists
+                                </th>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">
+                                    1. * <i>your text</i> * <br />
+                                    2. * <i>your text</i> * <br />
+                                    3. * <i>your text</i> *
+                                </td>
+                                <td><div className="text-item"><ol><li>Your text</li><li>Your text</li><li>Your text</li></ol></div></td>
+                            </tr>
+                            <tr>
+                                <td onClick={props.onClick} className="tag">
+                                    - * <i>your text</i> * <br />
+                                    - * <i>your text</i> * <br />
+                                    - * <i>your text</i> *
+                                </td>
+                                <td><div className="text-item"><ul><li>Your text</li><li>Your text</li><li>Your text</li></ul></div></td>
+                            </tr>                       
+                        </table>
                     </OffcanvasBody>
                 </Offcanvas>
             </div>
